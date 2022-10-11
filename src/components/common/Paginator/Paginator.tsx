61dc68953414ac9b1aa2629223e7f13a10557318
EvgenySychev <samara808@yandex.ru>
2022-10-11 11:03:16 +0400

    add Paginator styles

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -9,7 +9,13 @@ type PaginatorPropsType = {
     portionSize?: number
 }
 
-export const Paginator = ({totalItemsCount, pageSize, currentPages, onPageChanged, portionSize = 20}: PaginatorPropsType) => {
+export const Paginator = ({
+                              totalItemsCount,
+                              pageSize,
+                              currentPages,
+                              onPageChanged,
+                              portionSize = 10
+                          }: PaginatorPropsType) => {
 
 
     let pagesCount = Math.ceil(totalItemsCount / pageSize)
@@ -21,25 +27,36 @@ export const Paginator = ({totalItemsCount, pageSize, currentPages, onPageChange
     let portionCount = Math.ceil(pagesCount / portionSize)
     let [portionNumber, setPortionNumber] = useState(1)
     let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
-    let rightPortionPageNumber  = portionNumber * portionSize
+    let rightPortionPageNumber = portionNumber * portionSize
 
     return (
 
         <div className={style.paginator}>
-            {portionNumber > 1 &&
-                <button onClick={()=> {setPortionNumber(portionNumber-1)}}>PREV</button>}
+            <div className={style.btnblock}>
+                {portionNumber > 1 &&
+                    <button className={style.prevbtn} onClick={() => {
+                        setPortionNumber(portionNumber - 1)
+                    }}>PREV</button>}
+            </div>
 
-            {pages
-                .filter(p => p>= leftPortionPageNumber && p<= rightPortionPageNumber)
-                .map(p => {
-                return <span
-                    className={style.pageNumber} //currentPages приходит undefined
-                    //здесь надо пофиксить, спан жирным не рисует
-                    onClick={(e) => onPageChanged(p)}>{p}
+            <div>
+                {pages
+                    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                    .map(p => {
+                        return <span
+                            className={style.pageNumber} //currentPages приходит undefined
+                            //здесь надо пофиксить, спан жирным не рисует
+                            onClick={(e) => onPageChanged(p)}>{p}
                     </span>
-            })}
-            {portionCount > portionNumber &&
-                <button onClick={()=>{ setPortionNumber(portionNumber + 1)}}>NEXT</button>}
+                    })}
+            </div>
+            <div className={style.btnblock}>
+                {portionCount > portionNumber &&
+                    <button className={style.nextbtn}  onClick={() => {
+                        setPortionNumber(portionNumber + 1)
+                    }}>NEXT</button>}
+            </div>
+
         </div>
     )
 }
