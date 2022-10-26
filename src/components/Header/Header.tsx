63f6ebb69c370dcfa54f03cc9d7c6beaad653374
EvgenySychev <samara808@yandex.ru>
2022-10-26 22:53:31 +0400

    added userPhoto in Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import s from './Header.module.scss' ;
 import {logoutTC} from "../../redux/auth-reducer";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import React from "react";
 import {HeaderLogo} from "./HeaderLogo";
+import userPhoto from "../../assets/images/user.png";
+import {AppStateType} from "../../redux/redux-store";
+import {ProfileType} from "../../redux/profile-reducer";
 
 const Header = (props: any) => {
 
+    let ownerProfile = useSelector<AppStateType, ProfileType>(state => state.profilePage.ownerProfile)
     const dispatch = useDispatch()
 
     const logout = () => {
@@ -17,10 +21,15 @@ const Header = (props: any) => {
             <HeaderLogo/>
             <div className={s.loginBlock}>
                 {props.isAuth
-                    ? (<div style={{color: 'black'}}>{props.login}
+                    ? (<div className={s.loginInfo}>
+                        <p>{props.login}</p>
+                        <img style={{height: '35px', width: '35px'}}
+                             src={ownerProfile.photos?.small || userPhoto}
+                             alt="ownerPhoto"/>
                         <button onClick={logout}>LOGOUT</button>
                     </div>)
-                    : <button onClick={()=>alert('there will be a transition to register page soon')}>REGISTER</button>
+                    : <button
+                        onClick={() => alert('there will be a transition to register page soon')}>REGISTER</button>
                 }
             </div>
         </div>
