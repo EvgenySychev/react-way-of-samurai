f1233ee2e201fa91599581d66573c3abff29c2b6
EvgenySychev <samara808@yandex.ru>
2022-11-05 22:09:29 +0400

    added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,54 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from 'react';
+
 import './App.css';
-import {Login} from "./components/Login/Login";
-import {useDispatch, useSelector} from "react-redux";
-import {AppStateType} from "./redux/redux-store";
-import {Preloader} from "./common/components/Preloader/Preloader";
-import {getAuthUserData} from "./redux/auth-reducer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Content} from "./components/Content/Content";
+import { useDispatch, useSelector } from 'react-redux';
 
+import { Preloader } from './common/components/Preloader/Preloader';
+import { Content } from './components/Content/Content';
+import HeaderContainer from './components/Header/HeaderContainer';
+import { Login } from './components/Login/Login';
+import { Navbar } from './components/Navbar/Navbar';
+import { getAuthUserData } from './redux/auth-reducer';
+import { AppStateType } from './redux/redux-store';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const App = () => {
+  const initialized = useSelector<AppStateType, boolean>(state => state.app.initialized);
+  const isAuth = useSelector<AppStateType, boolean>(state => state.auth.isAuth);
 
-    const initialized = useSelector<AppStateType, boolean>(state => state.app.initialized)
-    const isAuth = useSelector<AppStateType, boolean>(state => state.auth.isAuth)
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const catchAllUnhandleErrors = () => {
+    return alert('Some error occured');
+  };
 
-    const catchAllUnhandleErrors = (promiseRejectionEvent: any) => {
-        alert("Some error occured")
-    }
+  useEffect(() => {
+    dispatch(getAuthUserData());
+    window.addEventListener('unhandledrejection', catchAllUnhandleErrors);
 
-    useEffect(() => {
-        dispatch(getAuthUserData())
-        window.addEventListener("unhandledrejection", catchAllUnhandleErrors)
-        return () => {
-            window.removeEventListener("unhandledrejection", catchAllUnhandleErrors)
-        }
-    }, [])
+    return () => {
+      window.removeEventListener('unhandledrejection', catchAllUnhandleErrors);
+    };
+  }, []);
 
-    if (!initialized) {
-        return <Preloader/>
-    }
+  if (!initialized) {
+    return <Preloader />;
+  }
 
-    return <div className='app-wrapper-content'>
-        <HeaderContainer/>
-        {!isAuth
-            ? <Login/>
-            : <div className='app-wrapper'>
-                <Navbar/>
-                <Content/>
-            </div>}
+  return (
+    <div className="app-wrapper-content">
+      <HeaderContainer />
+      {!isAuth ? (
+        <Login />
+      ) : (
+        <div className="app-wrapper">
+          <Navbar />
+          <Content />
+        </div>
+      )}
     </div>
-}
+  );
+};
 
 export default App;
